8f3ecd06b462a9d906be1e6d9735286e56c42d0f
Darren Wood <darren.wood@thomsonreuters.com>
2018-11-11 17:01:32 +0000

    Added 2015-12-03

---
+++ 2015/2015_03/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2015_03
+{
+    class Program
+    {
+        static Dictionary<string, int> visitedHouses = new Dictionary<string, int>();
+
+        static void Main(string[] args)
+        {
+            int sX = 0;
+            int sY = 0;
+            int rSX = 0;
+            int rSY = 0;
+            bool roboSanta = false;
+            string line;
+
+            visitedHouses.Add("0,0", 2);
+
+            System.IO.StreamReader file = new System.IO.StreamReader("input");
+            while ((line = file.ReadLine()) != null)
+            {
+                foreach (char direction in line)
+                {
+                    if (!roboSanta){
+                        switch (direction)
+                        {
+                            case '^':
+                                sY++;
+                                break;
+                            case '>':
+                                sX++;
+                                break;
+                            case 'v':
+                                sY--;
+                                break;
+                            case '<':
+                                sX--;
+                                break;
+                        }
+
+                        IncrementValue(visitedHouses, GetCoord(sX, sY));
+                    }
+                    else
+                    {
+                        switch (direction)
+                        {
+                            case '^':
+                                rSY++;
+                                break;
+                            case '>':
+                                rSX++;
+                                break;
+                            case 'v':
+                                rSY--;
+                                break;
+                            case '<':
+                                rSX--;
+                                break;
+                        }
+
+                        if (!((rSX == 0) && (rSY == 0)))
+                        {
+                            IncrementValue(visitedHouses, GetCoord(rSX, rSY));
+                        }
+                    }
+                    roboSanta = !roboSanta;
+                }
+            }
+
+            Console.WriteLine($"{visitedHouses.Count} houses have at least one present");
+        }
+
+        static string GetCoord(int x, int y){
+            return x.ToString() + ',' + y.ToString();
+        }
+
+        static void IncrementValue(Dictionary<string,int> dic, string key){
+            int val = 1;
+            if (dic.ContainsKey(key)){
+                dic.TryGetValue(key, out val);
+                dic.Remove(key);
+                val++;
+            }
+            dic.Add(key, val);
+        }
+    }
+}
