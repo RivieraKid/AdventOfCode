92229f6d5b5b49976e9aa266401d575461b2a360
Darren Wood <darren.wood@thomsonreuters.com>
2018-11-11 18:49:15 +0000

    Added 2015-12-04

---
+++ 2015/2015_04/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace _2015_04
+{
+    class Program
+    {
+        static string input = "iwrupvqb";
+        static Int64 count = 0;
+
+        static void Main(string[] args)
+        {
+            byte[] md5;
+            string cksum = "";
+
+            // For part two, just change the string below to "000000"
+            while (!(cksum.StartsWith("00000"))){
+                count++;
+                md5 = ComputeMD5(input + count.ToString());
+                cksum = ByteArrayToString(md5);
+            }
+            Console.WriteLine($"Count is {count}, MD5 is {cksum}");
+        }
+
+        static byte[] ComputeMD5(string str){
+            using (var md5 = MD5.Create()){
+                var result = md5.ComputeHash(Encoding.ASCII.GetBytes(str));
+                return result;
+            }
+        }
+
+        static string ByteArrayToString(byte[] val){
+            var ret = BitConverter.ToString(val).Replace("-", "");
+            return ret;
+        }
+    }
+}
